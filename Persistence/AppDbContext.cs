829f01a2ed535a2aa839a31e82e8050d83f30b97
jonk <noxxan@protonmail.com>
2025-09-22 20:04:28 +0200

    End of section 2

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,14 @@
+
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+    public required DbSet<Activity> Activities { get; set; }
+}
